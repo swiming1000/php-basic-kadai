2b936037fcef0f230c7bbad841bda6232b26faa7
swiming1000 <swiming1000@yahoo.co.jp>
2025-06-07 18:48:31 +0900

    kadai_007をアップロードしました

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        $vegetable_data = ['name'=>'onion','price'=>200,'weight'=>160];
+        print_r($vegetable_data);
+        ?>
+    </p>
+    
+</body>
+</html>
